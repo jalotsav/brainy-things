f50730fea983947059795281c708869a4527ab9a
ManishKarena <@pushit1@>
2018-10-27 12:28:28 +0530

    android: Add in KOTLIN language - Add new directory for kotlin and implement kotlin code

---
+++ android/kotlin/BrainyThings/things/src/main/java/com/jalotsav/brainythings/models/MdlTraits.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018 Jalotsav
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jalotsav.brainythings.models;
+
+/**
+ * Created by Jalotsav on 2/27/2018.
+ */
+
+public class MdlTraits {
+
+    boolean isOn;
+    int interval;
+
+    public MdlTraits() {
+    }
+
+    public MdlTraits(boolean isOn, int interval) {
+        this.isOn = isOn;
+        this.interval = interval;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+}
