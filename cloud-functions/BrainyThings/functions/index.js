92cb340aa67ed492f1525857d1ee8198e57c6fb3
Jaldeep Asodariya <jdpatel.android@gmail.com>
2019-02-01 11:01:53 +0530

    cloudfunctions: Migrate to Dialogflow API V2 * Migrate from Dialogflow API V1 to API V2

--- cloud-functions/BrainyThings/functions/index.js
+++ cloud-functions/BrainyThings/functions/index.js
@@ -1,16 +1,21 @@
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
-
+const {
+    dialogflow,
+    BasicCard,
+    Suggestions,
+    Image,
+} = require('actions-on-google');
 const functions = require('firebase-functions');
-const { DialogflowApp } = require('actions-on-google');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-var sprintf = require('sprintf-js').sprintf;
+admin.initializeApp();
 
+var sprintf = require('sprintf-js').sprintf;
 const strings = require('./strings') // Import strings.js for use constants value
 
-// Firebase Realtime database reference
+const app = dialogflow({ debug: true });
+
+// Firebase Realtime database references
 const jalosavHomeRef = admin.database().ref(strings.general.fireDatabase.rootName);
 const roomsRef = jalosavHomeRef.child(strings.general.fireDatabase.childRooms);
 
@@ -20,115 +25,107 @@ const roomsRef = jalosavHomeRef.child(strings.general.fireDatabase.childRooms);
  */
 const getRandomValue = array => array[Math.floor(Math.random() * array.length)];
 
-exports.thingsExecute = functions.https.onRequest((request, response) => {
+var isRoomMatched = false;
+var isDeviceMatched = false;
 
-    const assistant = new DialogflowApp({ request: request, response: response });
+app.intent("things_execute", (conv, params) => {
+    const paramThingsRooms = params[strings.dialogflowConsole.parameter.thingsRooms];
+    const paramThingsDevices = params[strings.dialogflowConsole.parameter.thingsDevices];
+    const paramThingsTraits = params[strings.dialogflowConsole.parameter.thingsTraits];
+    let fulfillmentMsg = strings.general.unhandled;
+    let deviceTraitsIsOn;
 
-    // The Entry point to all our actions
-    const actionMap = new Map();
-    actionMap.set(strings.dialogflowConsole.action.thingsExecute, executeHandler);
-    assistant.handleRequest(actionMap);
+    // Get boolean value of Device Traits isOn by comparing on/off string
+    if (paramThingsTraits === strings.general.onSml)
+        deviceTraitsIsOn = true;
+    else if (paramThingsTraits === strings.general.offSml)
+        deviceTraitsIsOn = false;
 
-    // Fulfill things execute action business logic
-    function executeHandler(assistant) {
+    return roomsRef.once('value', snapshot => {
+        snapshot.forEach(function (childSnapshot) {
+            if (paramThingsRooms === childSnapshot.val().name.toLowerCase()) {
+                isRoomMatched = true;
+                console.log('Room name matched.');
 
-        const screenOutput = assistant.hasSurfaceCapability(assistant.SurfaceCapabilities.SCREEN_OUTPUT);
-        const paramThingsRooms = assistant.getArgument(strings.dialogflowConsole.parameter.thingsRooms);
-        const paramThingsDevices = assistant.getArgument(strings.dialogflowConsole.parameter.thingsDevices);
-        const paramThingsTraits = assistant.getArgument(strings.dialogflowConsole.parameter.thingsTraits);
-        let fulfillmentMsg = strings.general.unhandled;
-        let deviceTraitsIsOn;
+                // Get all devices of matched room
+                var devicesSnapshot = childSnapshot.child(strings.general.fireDatabase.childDevices);
 
-        // Get boolean value of Device Traits isOn by comparing on/off string
-        if (paramThingsTraits === strings.general.onSml)
-            deviceTraitsIsOn = true;
-        else if (paramThingsTraits === strings.general.offSml)
-            deviceTraitsIsOn = false;
+                devicesSnapshot.forEach(function (childDeviceSnapshot) {
+                    if (paramThingsDevices === childDeviceSnapshot.val().name.toLowerCase()) {
+                        isDeviceMatched = true;
+                        console.log('Device name matched');
 
-        console.log('Room Name: ', paramThingsRooms);
-        console.log('Device Name: ', paramThingsDevices);
-        console.log('Traits: ', paramThingsTraits);
+                        let fireDBTraitsIsOn = childDeviceSnapshot.val().traits.isOn;
+                        console.log('Device current traits isOn: ', fireDBTraitsIsOn);
 
-        // Get all Rooms from firebase database
-        roomsRef.once('value', snapshot => {
+                        if (fireDBTraitsIsOn !== deviceTraitsIsOn) { // Requested Traits isOn value is different than database value
+                            const traitsIsOnRef = roomsRef.child(childSnapshot.key)
+                                .child(strings.general.fireDatabase.childDevices)
+                                .child(childDeviceSnapshot.key)
+                                .child(strings.general.fireDatabase.childTraits);
 
-            snapshot.forEach(function (childSnapshot) {
+                            // Update Traits isOn value and generate response and send back to assistant
+                            return new Promise ((resolve, reject) => {
+                                traitsIsOnRef.update({ isOn: deviceTraitsIsOn, on: deviceTraitsIsOn });
 
-                if (paramThingsRooms === childSnapshot.val().name.toLowerCase()) {
+                                console.log('Device traits isOn udpated to: ', deviceTraitsIsOn);
 
-                    console.log('Room name matched.');
+                                fulfillmentMsg = sprintf(getRandomValue(strings.richResponse.fulfillmentTurning), paramThingsRooms, paramThingsDevices, paramThingsTraits);
 
-                    // Get all devices of matched room
-                    var devicesSnapshot = childSnapshot.child(strings.general.fireDatabase.childDevices);
-                    devicesSnapshot.forEach(function (childDeviceSnapshot) {
+                                /**
+                                 * If Assistant device has screen,
+                                 * Generate Rich response with BasicCard including Title, Text, Button, Image and Suggestion chips
+                                 */
+                                if (!conv.screen)
+                                    conv.ask(fulfillmentMsg);
+                                else {
+                                    let [imagePath, imageName] = ["", ""];
+                                    if (deviceTraitsIsOn)
+                                        [imagePath, imageName] = strings.richResponse.images.lightOn;
+                                    else
+                                        [imagePath, imageName] = strings.richResponse.images.lightOff;
 
-                        if (paramThingsDevices === childDeviceSnapshot.val().name.toLowerCase()) {
+                                    conv.ask(fulfillmentMsg, new BasicCard({
+                                        title: sprintf(strings.general.cardTitleTurning, paramThingsTraits),
+                                        text: fulfillmentMsg,
+                                        image: new Image({
+                                            url: imagePath,
+                                            alt: imageName,
+                                        }),
+                                    }));
+                                    conv.ask(new Suggestions(deviceTraitsIsOn ? strings.richResponse.suggestions.turnOff : strings.richResponse.suggestions.turnOn));
+                                }
+                                resolve();
+                            });
+                        } else { // Device is already On/Off
+                            console.log('Device traits isOn is already ', deviceTraitsIsOn);
 
-                            console.log('Device name matched');
-
-                            let fireDBTraitsIsOn = childDeviceSnapshot.val().traits.isOn;
-                            console.log('Device current traits isOn: ', fireDBTraitsIsOn);
-
-                            if (fireDBTraitsIsOn !== deviceTraitsIsOn) { // Requested Traits isOn value is different than database value
-
-                                const traitsIsOnRef = roomsRef.child(childSnapshot.key)
-                                    .child(strings.general.fireDatabase.childDevices)
-                                    .child(childDeviceSnapshot.key)
-                                    .child(strings.general.fireDatabase.childTraits);
-
-                                // Update Traits isOn value and generate response and send back to assistant
-                                traitsIsOnRef.update({ isOn: deviceTraitsIsOn, on: deviceTraitsIsOn }).then(function () {
-
-                                    console.log('Device traits isOn change to ', deviceTraitsIsOn);
-                                    fulfillmentMsg = sprintf(getRandomValue(strings.richResponse.fulfillmentTurning), paramThingsRooms, paramThingsDevices, paramThingsTraits);
-
-                                    /**
-                                     * If Assistant device has screen,
-                                     * Generate RichResponse with Text, Card (with Image) and suggestion chips
-                                     */
-                                    if (screenOutput) {
-
-                                        let [imagePath, imageName] = ["", ""];
-                                        if (deviceTraitsIsOn)
-                                            [imagePath, imageName] = strings.richResponse.images.lightOn;
-                                        else
-                                            [imagePath, imageName] = strings.richResponse.images.lightOff;
-
-                                        const card = assistant.buildBasicCard(sprintf(strings.general.cardTitleTurning, paramThingsTraits))
-                                            .setImage(imagePath, imageName);
-
-                                        const richResponseBuild = assistant.buildRichResponse()
-                                            .addSimpleResponse(fulfillmentMsg)
-                                            .addBasicCard(card)
-                                            .addSuggestions(deviceTraitsIsOn ? strings.richResponse.suggestions.turnOff : strings.richResponse.suggestions.turnOn);
-
-                                        assistant.ask(richResponseBuild);
-                                    } else
-                                        assistant.ask(fulfillmentMsg);
-
-                                    return traitsIsOnRef;
-                                }).catch(function (error) {
-                                    console.error(error);
-                                });
-                            } else { // Device is already On/Off
-
-                                console.log('Device traits isOn is already ', deviceTraitsIsOn);
-                                fulfillmentMsg = sprintf(getRandomValue(strings.richResponse.fulfillmentTurnAlready), paramThingsRooms, paramThingsDevices, paramThingsTraits);
-                                
-                                if(screenOutput) {
-
-                                    const richResponseBuild = assistant.buildRichResponse()
-                                        .addSimpleResponse(fulfillmentMsg)
-                                        .addSuggestions(deviceTraitsIsOn ? strings.richResponse.suggestions.turnOff : strings.richResponse.suggestions.turnOn);
-                
-                                    assistant.ask(richResponseBuild);
-                                } else 
-                                    assistant.ask(fulfillmentMsg);
-                            }
+                            fulfillmentMsg = sprintf(getRandomValue(strings.richResponse.fulfillmentTurnAlready), paramThingsRooms, paramThingsDevices, paramThingsTraits);
+                            conv.ask(fulfillmentMsg);
+                            if (conv.screen)
+                                conv.ask(new Suggestions(deviceTraitsIsOn ? strings.richResponse.suggestions.turnOff : strings.richResponse.suggestions.turnOn));
                         }
-                    });
-                }
-            });
+
+                        return conv; // return for STOP the loop.
+                    } else 
+                        isDeviceMatched = false;
+                });
+
+                return conv; // return for STOP the loop.
+            } else 
+                isRoomMatched = false;
         });
-    }
+
+        if (!isRoomMatched) {
+            console.log('Room name does NOT matched.');
+            conv.ask(`Oops! we didn't find this room in your home.`);
+        } else {
+            if (!isDeviceMatched) {
+                console.log('Device name does NOT matched');
+                conv.ask(`Oops! we didn't find this device in the room.`);
+            }
+        }
+    });
 });
+
+exports.brainyThingsWebhook = functions.https.onRequest(app);
